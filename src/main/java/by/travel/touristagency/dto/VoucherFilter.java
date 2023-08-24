b751b3aa28a0834daaa4e6a5f2faf30878cceb89
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-08-25 00:09:40 +0300

    feat: realize the possibility to get a voucher using VoucherFilter.java. A VoucherFilter.java is created by the user

--- src/main/java/by/travel/touristagency/dto/VoucherFilter.java
+++ src/main/java/by/travel/touristagency/dto/VoucherFilter.java
@@ -1,5 +1,6 @@
 package by.travel.touristagency.dto;
 
+import by.travel.touristagency.entity.enums.Country;
 import by.travel.touristagency.entity.enums.Transport;
 import by.travel.touristagency.entity.enums.Type;
 import lombok.Builder;
@@ -13,7 +14,7 @@ public class VoucherFilter {
     Double priceTo;
     Type type;
 
-    String country;
+    Country country;
     Integer amountOfDays;
     Transport transport;
     boolean food;
--- src/main/java/by/travel/touristagency/repository/VoucherRepository.java
+++ src/main/java/by/travel/touristagency/repository/VoucherRepository.java
@@ -1,11 +1,39 @@
 package by.travel.touristagency.repository;
 
+import by.travel.touristagency.dto.VoucherFilter;
 import by.travel.touristagency.entity.Voucher;
+import by.travel.touristagency.entity.enums.Country;
+import by.travel.touristagency.entity.enums.Transport;
+import by.travel.touristagency.entity.enums.Type;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.jpa.impl.JPAQuery;
 import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+import static by.travel.touristagency.entity.QVoucher.voucher;
 
 public class VoucherRepository extends BaseRepository<Long, Voucher> {
 
     public VoucherRepository(EntityManager entityManager) {
         super(Voucher.class, entityManager);
+    }
+
+    public List<Voucher> getFilteredVoucher(VoucherFilter filter) {
+        Predicate predicate = CustomPredicate.builder()
+                .add(filter.getName(), voucher.name::eq)
+                .add(filter.getPriceFrom(), voucher.price::goe)
+                .add(filter.getPriceTo(), voucher.price::loe)
+                .add(Type.valueOf(filter.getType().name()), voucher.type::eq)
+                .add(Country.valueOf(filter.getCountry().name()), voucher.info.country::eq)
+                .add(Transport.valueOf(filter.getTransport().name()), voucher.info.transport::eq)
+                .add(filter.isFood(), voucher.info.isFoodInclude::eq)
+                .buildAnd();
+
+        return new JPAQuery<Voucher>(getEntityManager())
+                .select(voucher)
+                .from(voucher)
+                .where(predicate)
+                .fetch();
     }
 }
