a8d3ea855b433f0cd165641e24e7659a2c580e81
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-08-25 00:50:28 +0300

    feat: realize sorting by price & food

--- src/main/java/by/travel/touristagency/repository/VoucherRepository.java
+++ src/main/java/by/travel/touristagency/repository/VoucherRepository.java
@@ -20,7 +20,51 @@ public class VoucherRepository extends BaseRepository<Long, Voucher> {
     }
 
     public List<Voucher> getFilteredVoucher(VoucherFilter filter) {
-        Predicate predicate = CustomPredicate.builder()
+        Predicate predicate = buildPredicate(filter);
+
+        return new JPAQuery<Voucher>(getEntityManager())
+                .select(voucher)
+                .from(voucher)
+                .where(predicate)
+                .fetch();
+    }
+
+    public List<Voucher> getVouchersSortedByPriceASC() {
+        return new JPAQuery<Voucher>(getEntityManager())
+                .select(voucher)
+                .from(voucher)
+                .orderBy(voucher.price.asc())
+                .fetch();
+    }
+
+    public List<Voucher> getVouchersSortedByPriceDESC() {
+
+        return new JPAQuery<Voucher>(getEntityManager())
+                .select(voucher)
+                .from(voucher)
+                .orderBy(voucher.price.desc())
+                .fetch();
+    }
+
+    public List<Voucher> getVouchersSortedByFoodASC() {
+        return new JPAQuery<Voucher>(getEntityManager())
+                .select(voucher)
+                .from(voucher)
+                .orderBy(voucher.info.isFoodInclude.asc())
+                .fetch();
+    }
+
+    public List<Voucher> getVouchersSortedByFoodDESC() {
+
+        return new JPAQuery<Voucher>(getEntityManager())
+                .select(voucher)
+                .from(voucher)
+                .orderBy(voucher.info.isFoodInclude.desc())
+                .fetch();
+    }
+
+    private static Predicate buildPredicate(VoucherFilter filter) {
+        return CustomPredicate.builder()
                 .add(filter.getName(), voucher.name::eq)
                 .add(filter.getPriceFrom(), voucher.price::goe)
                 .add(filter.getPriceTo(), voucher.price::loe)
@@ -29,11 +73,5 @@ public class VoucherRepository extends BaseRepository<Long, Voucher> {
                 .add(Transport.valueOf(filter.getTransport().name()), voucher.info.transport::eq)
                 .add(filter.isFood(), voucher.info.isFoodInclude::eq)
                 .buildAnd();
-
-        return new JPAQuery<Voucher>(getEntityManager())
-                .select(voucher)
-                .from(voucher)
-                .where(predicate)
-                .fetch();
     }
 }
