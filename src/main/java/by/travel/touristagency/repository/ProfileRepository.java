7a10e32503aeb74927fa7b025bb0027996cd576d
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-08-30 18:57:58 +0300

    feat: add search by userId test: add test to getProfileByUserId()

--- src/main/java/by/travel/touristagency/repository/ProfileRepository.java
+++ src/main/java/by/travel/touristagency/repository/ProfileRepository.java
@@ -1,11 +1,26 @@
 package by.travel.touristagency.repository;
 
 import by.travel.touristagency.entity.Profile;
+import com.querydsl.jpa.impl.JPAQuery;
 import jakarta.persistence.EntityManager;
+
+import java.util.Optional;
+
+import static by.travel.touristagency.entity.QProfile.profile;
 
 public class ProfileRepository extends BaseRepository<Long, Profile> {
 
     public ProfileRepository(EntityManager entityManager) {
         super(Profile.class, entityManager);
+    }
+
+    public Optional<Profile> getProfileByUserId(Long userId) {
+        return Optional.ofNullable(
+                new JPAQuery<Profile>(getEntityManager())
+                        .select(profile)
+                        .from(profile)
+                        .where(profile.user.id.eq(userId))
+                        .fetchOne()
+        );
     }
 }
---
+++ src/test/java/by/travel/touristagency/repository/ProfileRepositoryTest.java
@@ -0,0 +1,53 @@
+package by.travel.touristagency.repository;
+
+import by.travel.touristagency.entity.Profile;
+import by.travel.touristagency.util.HibernateTestUtil;
+import by.travel.touristagency.util.TestDataImporter;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.*;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProfileRepositoryTest {
+    private static final SessionFactory SESSION_FACTORY = HibernateTestUtil.buildSessionFactory();
+    private ProfileRepository profileRepository;
+    private Session session;
+
+    @BeforeAll
+    public static void initDb() {
+        TestDataImporter.importData(SESSION_FACTORY);
+    }
+
+    @AfterAll
+    public static void closeDb() {
+        SESSION_FACTORY.close();
+    }
+
+    @BeforeEach
+    public void openSession() {
+        session = SESSION_FACTORY.openSession();
+        profileRepository = new ProfileRepository(session);
+    }
+
+    @AfterEach
+    public void closeSession() {
+        session.close();
+    }
+
+    @Test
+    void getProfileByUserId() {
+        session.beginTransaction();
+
+        Optional<Profile> profile = profileRepository.findById(1L);
+        assertThat(profile).isPresent();
+
+        Optional<Profile> actualResult = profileRepository.getProfileByUserId(1L);
+        assertThat(actualResult).isPresent();
+        assertThat(actualResult).isEqualTo(profile);
+
+        session.getTransaction().commit();
+    }
+}
