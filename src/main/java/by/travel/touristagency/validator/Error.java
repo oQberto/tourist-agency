6159bb461da3c1e41ace8ec1e325673bb2fa3353
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-09-16 15:03:32 +0300

    feat: add abstract errors handler

---
+++ src/main/java/by/travel/touristagency/validator/Error.java
@@ -0,0 +1,9 @@
+package by.travel.touristagency.validator;
+
+import lombok.Getter;
+
+@Getter
+public class Error {
+    private String code;
+    private String message;
+}
---
+++ src/main/java/by/travel/touristagency/validator/ValidationResult.java
@@ -0,0 +1,19 @@
+package by.travel.touristagency.validator;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class ValidationResult {
+    private final List<Error> errors = new ArrayList<>();
+
+    public void add(Error error) {
+        errors.add(error);
+    }
+
+    public boolean isValid() {
+        return errors.isEmpty();
+    }
+}
